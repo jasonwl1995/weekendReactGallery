7f7809de6aeefbb33ddccd3893785385390a5408
Jason Lin <jasonwl_1995@yahoo.com>
2021-02-21 05:52:10 -0600

    Gallery Data set up

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, title:'Goat', path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, title:'Cooking', path: 'images/Cooking.jpg', description: 'Favorite hobby that has endless possibilities and creativity can run free.', likes: 0 },
+    { id: 3, title:'Music Festivals', path: 'images/EDMStage.jpg', description: 'The only traveling I have done recently are to music festivals.', likes: 0 },
+    { id: 4, title:'Movies', path: 'images/MovieTheater.jpg', description: 'Always going to movies when something good comes out.', likes: 0 },
+    { id: 5, title:'Soccer', path: 'images/Soccer.jpg', description: 'Favorite sport to play during the summer with friends.', likes: 0 },
+    { id: 6, title:'Valorant', path: 'images/Valorant.jpg', description: 'Favorite game to play when relaxing at home.', likes: 0 }
 ];
 
 module.exports = galleryItems;
