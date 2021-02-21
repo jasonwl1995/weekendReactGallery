a80b18c85111f6c4a078a31d67c6196663101532
Jason Lin <jasonwl_1995@yahoo.com>
2021-02-21 06:20:35 -0600

    Gallery outline set up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import GalleryList from '../GalleryList/GalleryList';
+import GalleryItem from '../GalleryItem/GalleryItem'
 
 function App() {
+  const [galleryList, setGalleryList] = useState([]);
+  const [] = useState();
+  const [] = useState();
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = () => {
+    axios.get('/gallery')
+      .then(response => {
+        console.log('got a response!', response.data);
+        setGalleryList(response.data);
+      })
+      .catch(err => {
+        console.log('There seems to be an error', err);
+      });
+  };
+
+
     return (
       <div className="App">
         <header className="App-header">
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,16 @@
+function GalleryList({galleryList}) {
+  return (
+    <ul>
+    {galleryList.map((gallery) => {
+      return(
+        <li key={gallery. /* name */}>
+          {gallery./* name */}
+          {gallery./* name */}
+          {gallery./* name */}
+          {gallery./* name */}
+        </li>
+      );
+      })}
+    </ul>
+  );
+}
