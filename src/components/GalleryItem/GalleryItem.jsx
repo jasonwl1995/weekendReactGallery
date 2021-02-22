1578123b1923fe99ce08687b1f743d4498a34fcf
Jason Lin <jasonwl_1995@yahoo.com>
2021-02-21 22:21:31 -0600

    onclick image changes flag value

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -5,12 +5,28 @@ function GalleryItem  ({
   item}) {  
   
   const [count, setCount] = useState(0);
+  const [imageFlag, setImageFlag] = useState(true);
+
 
 
     return (
       <span key={item.id}>
-          <span>
+          <span  onClick={()=>setImageFlag(!imageFlag)}>
+             flag value is {imageFlag?'111': item.description}
+             <Greeting flag={imageFlag}/>
             <img src= {item.path} width='100px' height='100px'></img>
+            
+            { //console.log('image flag is ', {imageFlag});
+              // if (imageFlag) {
+              //    getImage();
+              // }
+              // else
+              // {
+              //    getDescription();
+              // }
+
+               
+            }
             <div>
             <button onClick={() => {setCount(count + 1 ); likeCount(item.id);}}>love it!</button>
             </div>
@@ -31,6 +47,31 @@ const likeCount = (id) => {
     console.log('There is a like error', error);
   });
 };
+
+
+
+function getImage() {
+  return <h1>Welcome back!</h1>;
+}
+
+function getDescription() {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(flag) {
+  //const isLoggedIn = props.isLoggedIn;
+  console.log('inside function flag is', flag);
+  if (flag) {
+    return <h1>true flag</h1>;
+  }
+  return <h1>false Flag</h1>;
+}
+
+// ReactDOM.render(
+//   // Try changing to isLoggedIn={true}:
+//   <Greeting isLoggedIn={false} />,
+//   document.getElementById('root')
+// );
 
 export default GalleryItem;
 
